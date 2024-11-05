57f194897713e6d3a2f60d88d4deab9131ebe7d3
OleksandrShchur <sasha28shchur@gmail.com>
2024-11-05 20:50:18 +0100

    Initial commit

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,49 @@
+import { Global, css } from "@emotion/react"
+import { colors } from "./colors"
+
+const globalStyles = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: ${colors.primaryWhite};
+  }
+
+  body::-webkit-scrollbar {
+    width: 0; /* Скрыть скроллбар */
+    background: transparent; /* Дополнительно скрываем фон скроллбара */
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+  }
+
+  #root {
+    display: flex;
+    flex: 1;
+    height: 100%;
+  }
+
+  /* ::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  } */
+`
+
+export default function GlobalStyles() {
+  return <Global styles={globalStyles} />
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+interface Colors {
+  primaryWhite: string
+}
+
+export const colors: Colors = {
+  primaryWhite: "rgb(240, 240, 240)",
+}
